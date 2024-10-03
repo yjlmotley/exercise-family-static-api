f6d48783231828cc50a67b54cf42be78c57fbeac
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-10-03 13:25:38 -0700

    Update datastructures.py
    took out the quotes for the age as it should be integers and not strings

--- src/datastructures.py
+++ src/datastructures.py
@@ -17,21 +17,21 @@ class FamilyStructure:
         "id": 0,
         "first_name": "John",
         "last_name": self.last_name,
-        "age": "33",
+        "age": 33,
         "lucky_numbers": [7, 13, 22]
     },
     {
         "id": 1,
         "first_name": "Jane",
         "last_name": self.last_name,
-        "age": "35",
+        "age": 35,
         "lucky_numbers": [10, 14, 3]
     },
     {
         "id": 2,
         "first_name": "Jimmy",
         "last_name": self.last_name,
-        "age": "5",
+        "age": 5,
         "lucky_numbers": [1]
     }
 ]
